2ff0a722bc39961382f22047daec83a587066bb5
Maël Ehrhard <mael.ehrhard@outlook.fr>
2024-02-23 11:31:49 +0100

    🏗️ Vary speed of MovingObjects on each row

--- Game/Assets/Script/MovingObjectSpawner.cs
+++ Game/Assets/Script/MovingObjectSpawner.cs
@@ -9,9 +9,12 @@ public class MovingObjectSpawner : MonoBehaviour
     [SerializeField] private int minSeparationtime;
     [SerializeField] private int maxSeparationTime;
     [SerializeField] private bool isRightSide;
+
+    private float rowSpeed;
     // Start is called before the first frame update
     private void Start()
     {
+        rowSpeed = Random.Range(2.0f, 6.0f);
         StartCoroutine(SpawnVehicle());
     }
 
@@ -21,12 +24,11 @@ public class MovingObjectSpawner : MonoBehaviour
         {
             yield return new WaitForSeconds(Random.Range(minSeparationtime, maxSeparationTime));
             GameObject go = Instantiate(spawnObject, spawnPos.position, Quaternion.identity);
+            go.GetComponent<MovingObject>().Speed = rowSpeed;
             if(!isRightSide && go != null ) 
             {
                 go.transform.Rotate(new Vector3(0,180,0));
             }
-           
-
         }    
     }
     // Update is called once per frame
