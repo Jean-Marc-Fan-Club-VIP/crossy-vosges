9805be2af191bf9b7ae975345db6a7f6e8ed1177
charlineBx <charline.beaudeux8@etu.univ-lorraine.fr>
2024-03-06 07:42:26 +0100

    add a new continu song during a game

---
+++ Game/Assets/Script/AudioScript/DontDestroyOnLoadObj.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoadObj : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Ensure that this GameObject persists through scene changes
+        DontDestroyOnLoad(gameObject);
+    }
+}
