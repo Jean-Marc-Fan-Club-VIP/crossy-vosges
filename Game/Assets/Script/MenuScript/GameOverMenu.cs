ce7d9bc4a4dd071df897fc45fb6f94bf66a9ef23
charlineBx <charline.beaudeux8@etu.univ-lorraine.fr>
2024-03-13 18:54:56 +0100

    cut background music when game paused and game over

--- Game/Assets/Script/MenuScript/GameOverMenu.cs
+++ Game/Assets/Script/MenuScript/GameOverMenu.cs
@@ -7,15 +7,33 @@ using UnityEngine.UI;
 public class GameOverMenu : MonoBehaviour
 {
     public GameObject gameOverMenuUi;
+    private AudioControler audioController;
+
+    private void Start()
+    {
+        audioController = FindObjectOfType<AudioControler>();
+
+        if (audioController != null)
+        {
+            // Appeler la méthode PauseBackgroundMusic() de l'objet AudioControler
+            audioController.PauseBackgroundMusic();
+        }
+        else
+        {
+            Debug.LogWarning("AudioControler non trouvé dans la scène.");
+        }
+    }
 
     public void ReplayGame()
     {
+        audioController.ResumeBackgroundMusic();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
     public void QuitGame()
     {
-        Debug.Log("QUIT");
-        SceneManager.LoadScene(0);
+        audioController.ResumeBackgroundMusic();
+        Debug.Log("Back Start Menu");
+        SceneManager.LoadScene(1);
     }
 }
