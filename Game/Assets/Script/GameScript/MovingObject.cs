94144fedd3fff0fb7803a30d0974ba6955b46b06
charlineBx <charline.beaudeux8@etu.univ-lorraine.fr>
2024-04-12 17:35:45 +0200

    add changes for level 2 (speed car and night mode)

--- Game/Assets/Script/GameScript/MovingObject.cs
+++ Game/Assets/Script/GameScript/MovingObject.cs
@@ -8,7 +8,17 @@ public class MovingObject : MonoBehaviour
     public float rightBound;
 
     private void Update()
-    {
+    {   
+        int currentLevel = LevelSelector.LevelGame();
+        if (currentLevel == 1)
+        {
+            
+        }
+        else if (currentLevel == 2)
+        {
+            speed *= 1.003f;
+        }
+        
         transform.Translate(Vector3.forward * (speed * Time.deltaTime));
         var position = transform.position;
         if (position.z < leftBound || position.z > rightBound || position.y < -5)
---
+++ Game/Assets/Script/GameScript/NightModeManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightModeManager : MonoBehaviour
+{
+    public Light[] lights;
+    private static bool isNightMode = false;
+
+    private void Update()
+    {
+        int currentLevel = LevelSelector.LevelGame();
+        if (currentLevel == 1)
+        {
+            Debug.Log("LEVEL 1");
+            DesactivateNightMode();
+        }
+        else if (currentLevel == 2)
+        {
+            Debug.Log("LEVEL 2");
+            ActivateNightMode();
+        }
+    }
+
+    public void ActivateNightMode()
+    {
+        // Activer le brouillard dans la scène
+        RenderSettings.fog = true;
+        foreach (Light light in lights)
+        {
+            light.intensity *= 0.6f;
+        }
+        isNightMode = true;
+    }
+
+    public void DesactivateNightMode()
+    {
+        // Désactiver le brouillard dans la scène
+        RenderSettings.fog = false;
+        foreach (Light light in lights)
+        {
+            if (light.intensity < 1.25)
+            {
+                light.intensity /= 0.6f;
+            }
+        }
+        isNightMode = false;
+    }
+
+    public static bool IsNightModeActive()
+    {
+        return isNightMode;
+    }
+}
+
