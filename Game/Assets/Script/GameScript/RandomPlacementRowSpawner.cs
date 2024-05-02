ce012977f022917118eb539bfa539c6f7d00ad38
Maël Ehrhard <mael.ehrhard@outlook.fr>
2024-05-02 21:09:44 +0200

    ✨ Spawn coins

--- Game/Assets/Script/GameScript/RandomPlacementRowSpawner.cs
+++ Game/Assets/Script/GameScript/RandomPlacementRowSpawner.cs
@@ -3,15 +3,20 @@ using UnityEngine;
 
 public class RandomPlacementRowSpawner : MonoBehaviour
 {
-    public GameObject gameobject;
+    public GameObject spawnObject;
     public Transform spawnPos;
     public int minGameobject;
     public int maxGameobject;
+    [SerializeField] private int blockingLayer = 6;
+    private readonly Collider[] colliders = new Collider[1];
     private readonly List<int> positionsAlreadyTaken = new();
+    private int layerMask;
     private int numberGameobject;
+    
     
     private void Start()
     {
+        layerMask = 1 << blockingLayer;
         numberGameobject = Random.Range(minGameobject, maxGameobject);
         AddObject(0);
         SpawnObject();
@@ -38,12 +43,17 @@ public class RandomPlacementRowSpawner : MonoBehaviour
         positionsAlreadyTaken.Add(posZ);
         var randomPosition = new Vector3(spawnPos.position.x, 0.5f, posZ);
         var randomRotation = Quaternion.Euler(0, Random.Range(0, 4) * 90, 0);
-        var go = Instantiate(gameobject, randomPosition, randomRotation);
-        var movingObject = go.GetComponent<MovingObject>();
+        Instantiate(spawnObject, randomPosition, randomRotation);
     }
     
-    private bool IsTaken(int pos)
+    private bool IsTaken(int posZ)
     {
-        return positionsAlreadyTaken.Contains(pos);
+        /*var position = spawnPos.position;
+        position.z = posZ;
+        var boxCollider = spawnObject.GetComponent<BoxCollider>();
+        var isColliding = Physics.OverlapBoxNonAlloc(position, boxCollider.bounds.extents, colliders,
+            Quaternion.identity, layerMask) != 0;
+        Debug.Log($"isColliding: {isColliding} Position: {position} Extents: {boxCollider.bounds.extents}");*/
+        return positionsAlreadyTaken.Contains(posZ);
     }
 }
