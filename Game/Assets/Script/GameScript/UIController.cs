6e06a48366fbec373571139be27087bc389acf2b
Maël Ehrhard <mael.ehrhard@outlook.fr>
2024-05-02 09:08:41 +0200

    ♻️ Find child by name in UIController

--- Game/Assets/Script/GameScript/UIController.cs
+++ Game/Assets/Script/GameScript/UIController.cs
@@ -4,40 +4,46 @@ using UnityEngine;
 
 public class UIController : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI score;
-
-    [SerializeField] private TMP_Text _timer;
-
+    private TMP_Text score;
+    
+    private TMP_Text timer;
+    
+    private void Awake()
+    {
+        score = transform.Find("HLayout/Panel/Score").GetComponent<TMP_Text>();
+        timer = transform.Find("HLayout/Panel/Time").GetComponent<TMP_Text>();
+    }
+    
     private void Start()
     {
         EventManager.StartTimer();
     }
-
+    
     private void OnEnable()
     {
         EventManager.ScoreUpdated += EventManagerOnScoreUpdated;
         EventManager.TimerUpdated += EventManagerOnTimerUpdated;
         EventManager.GameOver += EventManagerOnGameOver;
     }
-
+    
     private void OnDisable()
     {
         EventManager.ScoreUpdated -= EventManagerOnScoreUpdated;
         EventManager.TimerUpdated -= EventManagerOnTimerUpdated;
         EventManager.GameOver -= EventManagerOnGameOver;
     }
-
+    
     private void EventManagerOnGameOver()
     {
         gameObject.SetActive(false);
     }
-
+    
     private void EventManagerOnTimerUpdated(float value)
     {
         var timeSpan = TimeSpan.FromSeconds(value);
-        _timer.text = timeSpan.ToString(@"mm\:ss");
+        timer.text = timeSpan.ToString(@"mm\:ss");
     }
-
+    
     private void EventManagerOnScoreUpdated(int value)
     {
         score.text = $"Score : {value}";
