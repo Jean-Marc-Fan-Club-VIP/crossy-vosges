94c9a1ba7695fca1a95a1daeece1a598d5321859
Maël Ehrhard <mael.ehrhard@outlook.fr>
2024-05-02 21:21:39 +0200

    🚧 WIP coins

---
+++ Game/Assets/Script/GameScript/CoinController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    private Transform coinChildElement;
+    
+    private void Start()
+    {
+        coinChildElement = transform.Find("CoinContainer");
+    }
+    
+    private void Update()
+    {
+        coinChildElement.transform.Rotate(0, Time.deltaTime * 100, 0);
+        coinChildElement.transform.position = new Vector3(coinChildElement.transform.position.x,
+            coinChildElement.transform.position.y + Mathf.Sin(Time.time * 2) * 0.001f,
+            coinChildElement.transform.position.z);
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.GetType() == typeof(Player))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
