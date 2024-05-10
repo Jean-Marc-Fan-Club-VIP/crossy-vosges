e2621269404a595c866abc92dce1f8826167c652
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2024-05-10 20:03:32 +0200

    Enhanced eagle animation

--- Game/Assets/Script/GameScript/EagleSpawner.cs
+++ Game/Assets/Script/GameScript/EagleSpawner.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class EagleSpawner : MonoBehaviour
 {
-    public GameObject eagle;
     public GameObject player;
     public AudioClip sound;
     private AudioSource audioSource;
 
     public static bool isReady;
-    private float speed = 6f;
+    private bool initialized = false;
     private bool soundIsPlayed = false;
+    private bool hasTouchedPlayer = false;
+    private bool flyingAnimation = false;
+    private bool animationFinished = false;
+
+    private const float LerpDesiredDurationMove = 1.5f;
+    private float lerpElapsedTime;
+    private Vector3 startPosition;
+    private Vector3 endPosition;
 
     void Start()
     {
-        transform.position = new Vector3(transform.position.x, 3f,  -1f);
+        startPosition = transform.position;
         isReady = false;
         audioSource = GetComponent<AudioSource>();
+        // Hide all renderers
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+        {
+            r.enabled = false;
+        }
     }
 
     void Update()
@@ -26,43 +38,97 @@ public class EagleSpawner : MonoBehaviour
         {
             if (isReady)
             {
-                PlayEagleSound();
+                if(!initialized)
+                {
+                    initialized = true;
+                    
+                    PlayEagleSound();
 
-                float deltaX = speed * Time.deltaTime;
-                transform.position += new Vector3(deltaX, 0f, 0f);
+                    // Show all renderers
+                    foreach (Renderer r in GetComponentsInChildren<Renderer>())
+                    {
+                        r.enabled = true;
+                    }
+
+                    transform.position = new Vector3(player.transform.position.x - 12f, player.transform.position.y + 8f, player.transform.position.z + 5f);
+                    startPosition = transform.position;
+                }
+            }
+            
+            if(!hasTouchedPlayer)
+            {
+                endPosition = new Vector3(player.transform.position.x, player.transform.position.y + 0.5f, player.transform.position.z);
             }
             else
             {
-                transform.position = new Vector3(player.transform.position.x - 12f, 3f, -1f);
+                if (!flyingAnimation)
+                {
+                    // Player touched
+                    flyingAnimation = true;
+
+                    // Fly
+                    startPosition = transform.position;
+                    lerpElapsedTime = 0;
+                    endPosition = new Vector3(transform.position.x + 6, transform.position.y + 8, transform.position.z + 10);
+
+                    // Get main camera
+                    Camera mainCamera = Camera.main;
+                    // Remove follow player script
+                    mainCamera.GetComponent<FollowPlayer>().enabled = false;
+                }
             }
-            //check if eagle has finished  its flight 
-            if (transform.position.x > player.transform.position.x + 9)
+
+            if(flyingAnimation)
             {
-                Destroy(gameObject);
+                // Attach player
+                player.GetComponent<Rigidbody>().useGravity = false;
+                player.transform.parent = transform;
+                player.transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+            }
+
+            // Move
+            if (startPosition != endPosition && isReady)
+            {
+                lerpElapsedTime += Time.deltaTime;
+                var percentageComplete = lerpElapsedTime / LerpDesiredDurationMove;
+                transform.position = Vector3.Lerp(startPosition, endPosition, percentageComplete);
+
+                if (!hasTouchedPlayer)
+                {
+                    // Rotation
+                    Vector3 rotation = transform.eulerAngles;
+                    rotation.x = Mathf.Sin(percentageComplete * 2 * Mathf.PI) * 50;
+                    rotation.z = percentageComplete * -45;
+                    transform.eulerAngles = rotation;
+                }
+                
+                if(flyingAnimation)
+                {
+                    // Rotation
+                    Vector3 rotation = transform.eulerAngles;
+                    rotation.x = percentageComplete * 50;
+                    rotation.z = percentageComplete * (30 + 45) - 45;
+                    transform.eulerAngles = rotation;
+                }
+
+                if (percentageComplete >= 1.0f)
+                {
+                    lerpElapsedTime = 0;
+                    startPosition = endPosition;
+
+                    hasTouchedPlayer = true;
+                    if(flyingAnimation)
+                    {
+                        animationFinished = true;
+                    }
+                }
+            }
+
+            // Kill player
+            if(animationFinished)
+            {
                 Destroy(player);
             }
-
-            // check if player and eagle is in same x position
-            int playerX = (int)player.transform.position.x;
-            int eagleX = (int)transform.position.x + 2;
-            if (Mathf.Approximately(playerX, eagleX))
-            {
-                SetPlayerTransparency(); 
-            }
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-        
-    }
-
-    private void SetPlayerTransparency()
-    {
-        Renderer[] renderers = player.GetComponentsInChildren<Renderer>();
-        foreach (Renderer renderer in renderers)
-        {
-            renderer.enabled = false;
         }
     }
 
