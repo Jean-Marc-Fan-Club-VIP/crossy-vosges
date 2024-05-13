83e612e1076ecd976275906b1a8a189c625fc616
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2024-05-13 13:58:37 +0200

    Fixed coin position and animation

--- Game/Assets/Script/GameScript/CoinController.cs
+++ Game/Assets/Script/GameScript/CoinController.cs
@@ -13,7 +13,7 @@ public class CoinController : MonoBehaviour
     {
         coinChildElement.transform.Rotate(0, Time.deltaTime * 100, 0);
         coinChildElement.transform.position = new Vector3(coinChildElement.transform.position.x,
-            coinChildElement.transform.position.y + Mathf.Sin(Time.time * 2) * 0.001f,
+            coinChildElement.transform.position.y + Mathf.Sin(Time.time * 2) * 0.002f * Time.timeScale,
             coinChildElement.transform.position.z);
     }
     
