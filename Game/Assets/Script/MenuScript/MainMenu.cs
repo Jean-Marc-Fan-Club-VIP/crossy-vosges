5c09b383476e729de926afd426142951b68e3e3e
Maël Ehrhard <mael.ehrhard@outlook.fr>
2024-05-16 11:04:10 +0200

    🩹 Give quit button a better name

--- Game/Assets/Script/MenuScript/MainMenu.cs
+++ Game/Assets/Script/MenuScript/MainMenu.cs
@@ -12,7 +12,7 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void QuitGame()
+    public void ExitGame()
     {
         Debug.Log("Game exited");
         Application.Quit();
